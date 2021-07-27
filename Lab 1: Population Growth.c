4036d80b36c389fea03ed46d5519c1a87a47f668
salma <77203008+salmakhaled74@users.noreply.github.com>
2021-07-27 23:26:50 +0200

    Create Lab 1: Population Growth.c

---
+++ Lab 1: Population Growth.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start_size;
+    do
+    {
+        start_size = get_int("Start Size: ");
+    }
+    while (start_size < 9);
+    
+    // TODO: Prompt for end size
+    int end_size;    
+    do
+    {
+        end_size = get_int("End Size: ");
+    }
+    while (end_size < start_size);
+    
+    // TODO: Calculate number of years until we reach threshold
+    int cnt = 0;
+    while (start_size < end_size)
+    {
+        start_size = start_size + (start_size / 3) - (start_size / 4);
+        cnt++;
+    }
+    // TODO: Print number of years
+    printf("Years: %d", cnt);
+}
